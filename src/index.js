ed71ed0bd2860d20f4ae81f16d53f5214578675c
Gabriela Arancibia <gabrielapaz.ae@gmail.com>
2024-05-23 19:50:51 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { connectDB } from './config/database.js';
 // Llamar a la función 'connectDB' para conectar con la base de datos
 connectDB();
 
-// Iniciar el servidor Express en el puerto 3000
-app.listen(3000);
-
-// Imprimir un mensaje en la consola para indicar que el servidor se ha iniciado en el puerto 3000
-console.log('Server on port', 3000);
+// Configurar el puerto y escuchar las solicitudes
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
