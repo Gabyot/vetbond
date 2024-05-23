296955ea7caa0e63d657941cebb472f50dc489e8
Gabriela Arancibia <gabrielapaz.ae@gmail.com>
2024-05-23 15:23:29 -0400

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,3 @@
-import { EventEmitterAsyncResource } from 'events';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
@@ -7,10 +6,7 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log(">>> DB is connected");
   } catch (error) {
     console.error(">>> Error connecting to the database:", error);
