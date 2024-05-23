b11a9610923cea0d0f744c82cea7266971e00ec8
Gabriela Arancibia <gabrielapaz.ae@gmail.com>
2024-05-23 13:49:28 -0400

    Update gitignore

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,18 @@
 import { EventEmitterAsyncResource } from 'events';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// Cargar variables de entorno desde el archivo .env
+dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://gabrielapazae:lsLDxgbcSc9YmFPG@authvetbond.ocwwkbd.mongodb.net/vetbond')
-    console.log(">>> DB is connected")
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(">>> DB is connected");
   } catch (error) {
+    console.error(">>> Error connecting to the database:", error);
   }
 };
