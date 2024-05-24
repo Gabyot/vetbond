ef5ec89e44e384e53c01e4442d0698a79ed45dcf
Gabriela Arancibia <gabrielapaz.ae@gmail.com>
2024-05-23 23:54:48 -0400

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -63,7 +63,7 @@ export const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true, // Previene el acceso al cookie mediante JavaScript
             secure: process.env.NODE_ENV === 'production', // Solo enviar la cookie a través de HTTPS en producción
-            sameSite: 'Strict', // Ajustar SameSite según tus necesidades ('Strict', 'Lax', 'None')
+            sameSite: 'None', // Ajustar SameSite según tus necesidades ('Strict', 'Lax', 'None')
             maxAge: 24 * 60 * 60 * 1000 // 1 día en milisegundos
         });
 
