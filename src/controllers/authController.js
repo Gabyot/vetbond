2ff97a6bf13167575164fd8d8bdeed5205f9f978
Gabriela Arancibia <gabrielapaz.ae@gmail.com>
2024-05-23 23:56:42 -0400

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -63,8 +63,9 @@ export const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true, // Previene el acceso al cookie mediante JavaScript
             secure: process.env.NODE_ENV === 'production', // Solo enviar la cookie a través de HTTPS en producción
-            sameSite: 'None', // Ajustar SameSite según tus necesidades ('Strict', 'Lax', 'None')
-            maxAge: 24 * 60 * 60 * 1000 // 1 día en milisegundos
+            sameSite: 'None', 
+            maxAge: 24 * 60 * 60 * 1000, // 1 día en milisegundos
+            domain: 'vetbond.render.com' 
         });
 
 
