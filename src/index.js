bbe4e8bbdf74f370604047338bd1bd469bc7d5ac
Gabriela Arancibia <gabrielapaz.ae@gmail.com>
2024-06-01 10:32:10 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import app from './app.js';
 import { connectDB } from './config/database.js';
+import express from 'express'
 
 // Llamar a la función 'connectDB' para conectar con la base de datos
 connectDB();
 
+app.use(express.static('../public'))
 // Configurar el puerto y escuchar las solicitudes
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
