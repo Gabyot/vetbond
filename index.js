de9e636f0d96c1b4fe4319f61c739e68d321f3d4
Gabriela Arancibia <gabrielapaz.ae@gmail.com>
2024-06-01 15:02:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 import app from './src/app.js';
 import express from 'express'
 import { connectDB } from './src/config/database.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-// Obtén la ruta del archivo actual
-const __filename = fileURLToPath(import.meta.url);
-
-// Obtén el directorio base
-const __dirname = path.dirname(__filename);
-
-app.use('/static', express.static(path.join(__dirname, 'public')));
 // Llamar a la función 'connectDB' para conectar con la base de datos
 connectDB();
 
