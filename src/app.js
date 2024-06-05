ce4051aba7bb1b2ce68577ba4f6d7a15b7e132bc
Gabriela Arancibia <gabrielapaz.ae@gmail.com>
2024-06-05 11:29:21 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Servir archivos estáticos desde la carpeta 'public'
-app.use(express.static(path.resolve(__dirname, '../public')));
+app.use(express.static(path.resolve('../public')));
 
 // Usar las rutas de autenticación bajo el prefijo '/api'
 app.use('/api', authRoutes);
