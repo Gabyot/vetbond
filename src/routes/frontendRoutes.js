b62d911a9a3f9403f817cdf0710933877c513f03
Gabriela Arancibia <gabrielapaz.ae@gmail.com>
2024-06-06 14:16:17 -0400

    Update frontendRoutes.js

--- src/routes/frontendRoutes.js
+++ src/routes/frontendRoutes.js
@@ -6,45 +6,46 @@ const router = Router();
 const staticRouter = express.Router();
 
 // Ruta para los archivos estáticos del frontend
-staticRouter.use(express.static(path.join('../../public')));
-
-// Ruta para el archivo HTML principal del frontend
-router.get('/', (req, res) => {
-    res.sendFile(path.resolve('public/pages/index.html'));
-});
-
-router.get('/services', (req, res) => {
-    res.sendFile(path.resolve('public/pages/services.html'));
-});
-
-router.get('/reserva', (req, res) => {
-    res.sendFile(path.resolve('public/pages/citasform.html'));
-});
-
-router.get('/login', (req, res) => {
-    res.sendFile(path.resolve('public/pages/login.html'));
-});
-router.get('/register', (req, res) => {
-    res.sendFile(path.resolve('public/pages/registro.html'));
-});
-
-router.get('/quienessomos', (req, res) => {
-    res.sendFile(path.resolve('public/pages/quienessomos.html'));
-});
-
-router.get('/profile', (req, res) => {
-    res.sendFile(path.resolve('public/pages/userProfile.html'));
-});
-
-router.get('/profile/review/:id', (req, res) => {
-    res.sendFile(path.resolve('public/pages/commentPage.html'));
-});
-
-router.get('/vetprofile/:id', (req, res) => {
-    res.sendFile(path.resolve('public/pages/vetProfile.html'));
-});
+staticRouter.use(express.static(path.resolve(__dirname, '../../public')));
 
 // Asociar el router de archivos estáticos con el router principal
 router.use(staticRouter);
+
+// Ruta para el archivo HTML principal del frontend
+router.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../public/pages/index.html'));
+});
+
+router.get('/services', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../public/pages/services.html'));
+});
+
+router.get('/reserva', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../public/pages/citasform.html'));
+});
+
+router.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../public/pages/login.html'));
+});
+
+router.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../public/pages/registro.html'));
+});
+
+router.get('/quienessomos', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../public/pages/quienessomos.html'));
+});
+
+router.get('/profile', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../public/pages/userProfile.html'));
+});
+
+router.get('/profile/review/:id', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../public/pages/commentPage.html'));
+});
+
+router.get('/vetprofile/:id', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../public/pages/vetProfile.html'));
+});
 
 export default router;
