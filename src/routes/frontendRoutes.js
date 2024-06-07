bb4e32c7b49a404e2fb8fb6fc7fac1a6bea72d24
Gabriela Arancibia <gabrielapaz.ae@gmail.com>
2024-06-07 15:55:26 -0400

    Update frontendRoutes.js

--- src/routes/frontendRoutes.js
+++ src/routes/frontendRoutes.js
@@ -6,46 +6,46 @@ const router = Router();
 const staticRouter = express.Router();
 
 // Ruta para los archivos estáticos del frontend
-staticRouter.use(express.static(path.resolve('../../public')));
+staticRouter.use(express.static(path.resolve('public')));
 
 // Asociar el router de archivos estáticos con el router principal
 router.use(staticRouter);
 
 // Ruta para el archivo HTML principal del frontend
 router.get('/', (req, res) => {
-    res.sendFile(path.resolve('../../public/pages/index.html'));
+    res.sendFile(path.resolve('public/pages/index.html'));
 });
 
 router.get('/services', (req, res) => {
-    res.sendFile(path.resolve('../../public/pages/services.html'));
+    res.sendFile(path.resolve('public/pages/services.html'));
 });
 
 router.get('/reserva', (req, res) => {
-    res.sendFile(path.resolve('../../public/pages/citasform.html'));
+    res.sendFile(path.resolve('public/pages/citasform.html'));
 });
 
 router.get('/login', (req, res) => {
-    res.sendFile(path.resolve('../../public/pages/login.html'));
+    res.sendFile(path.resolve('public/pages/login.html'));
 });
 
 router.get('/register', (req, res) => {
-    res.sendFile(path.resolve('../../public/pages/registro.html'));
+    res.sendFile(path.resolve('public/pages/registro.html'));
 });
 
 router.get('/quienessomos', (req, res) => {
-    res.sendFile(path.resolve('../../public/pages/quienessomos.html'));
+    res.sendFile(path.resolve('public/pages/quienessomos.html'));
 });
 
 router.get('/profile', (req, res) => {
-    res.sendFile(path.resolve('../../public/pages/userProfile.html'));
+    res.sendFile(path.resolve('public/pages/userProfile.html'));
 });
 
 router.get('/profile/review/:id', (req, res) => {
-    res.sendFile(path.resolve('../../public/pages/commentPage.html'));
+    res.sendFile(path.resolve('public/pages/commentPage.html'));
 });
 
 router.get('/vetprofile/:id', (req, res) => {
-    res.sendFile(path.resolve('../../public/pages/vetProfile.html'));
+    res.sendFile(path.resolve('public/pages/vetProfile.html'));
 });
 
 export default router;
